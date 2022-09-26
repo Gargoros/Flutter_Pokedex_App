c29c1668348772bde20061dc2bf54d5ffcf46256
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-26 22:22:18 +0300

    init main app function and main class

--- pokedex_application/lib/main.dart
+++ pokedex_application/lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_application/screens/homePage_screen.dart';
 
+void main() {
+  runApp(const PokedexApp());
+}
+
+class PokedexApp extends StatelessWidget {
+  const PokedexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Pokedex",
+      home: HomePageScreen(),
+    );
+  }
+}
