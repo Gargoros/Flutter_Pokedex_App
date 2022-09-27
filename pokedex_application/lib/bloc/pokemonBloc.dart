a1848a14200eabbfe284bc334d448f6bbf73990f
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-28 00:57:17 +0300

    init: create main bloc class

--- pokedex_application/lib/bloc/pokemonBloc.dart
+++ pokedex_application/lib/bloc/pokemonBloc.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
+import '../api/pokemonService.dart';
 import './pokemonState.dart';
 import './pokemonEvent.dart';
 
-class PokemonBloc extends Bloc<PokemonState, PokemonEvent> {
-  PokemonBloc() : super(null);
+class PokemonBloc extends Bloc<PokemonEvent, PokemonState> {
+  PokemonBloc() : super(PokemonInitialState()) {
+    on<PokemonEvent>(
+      (event, emit) async {
+        emit(PokemonLoadingState());
+        try {
+          final pokemonList = await PokemonService.fetchPokemonHub();
+          emit(PokemonLoadSuccessState(pokemonLis: pokemonList));
+        } catch (e) {
+          emit(PokemonLoadFaildState(errorMessage: e.toString()));
+        }
+      },
+    );
+  }
 }
