45eb694e0bedfe1d20290192c1b3ae7fc5971ace
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-28 00:27:54 +0300

    refactor: init new vars and constractors inside states

--- pokedex_application/lib/bloc/pokemonState.dart
+++ pokedex_application/lib/bloc/pokemonState.dart
@@ -1,9 +1,17 @@
+import '../models/pokemonModel.dart';
+
 abstract class PokemonState {}
 
 class PokemonInitialState extends PokemonState {}
 
 class PokemonLoadingState extends PokemonState {}
 
-class PokemonLoadSuccess extends PokemonState {}
+class PokemonLoadSuccessState extends PokemonState {
+  final List<Pokemon> pokemonLis;
+  PokemonLoadSuccessState({required this.pokemonLis});
+}
 
-class PokemonLoadFaild extends PokemonState {}
+class PokemonLoadFaildState extends PokemonState {
+  final String errorMessage;
+  PokemonLoadFaildState({required this.errorMessage});
+}
