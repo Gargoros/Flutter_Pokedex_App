4232c346d72231db8ddca18973807fbbfd0e8b27
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-28 00:21:07 +0300

    init new class that will tell us what's the state

--- pokedex_application/lib/bloc/pokemonState.dart
+++ pokedex_application/lib/bloc/pokemonState.dart
@@ -1,1 +1,9 @@
 abstract class PokemonState {}
+
+class PokemonInitialState extends PokemonState {}
+
+class PokemonLoadingState extends PokemonState {}
+
+class PokemonLoadSuccess extends PokemonState {}
+
+class PokemonLoadFaild extends PokemonState {}
