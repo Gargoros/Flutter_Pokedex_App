2c9d1112676281664c2f53e4eb603be446cc14ac
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-27 22:23:49 +0300

    init: create new screen for Single pokemon

---
+++ pokedex_application/lib/screens/singlePokemonPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SinglePokemonPage extends StatelessWidget {
+  const SinglePokemonPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
