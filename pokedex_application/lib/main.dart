a8269dada546df46c6bf0a7b8ec85d86c08e5598
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-28 01:10:51 +0300

    costumaze colors, textStyle

--- pokedex_application/lib/main.dart
+++ pokedex_application/lib/main.dart
@@ -13,6 +13,16 @@ class PokedexApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Pokedex",
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+              accentColor: Colors.black, primarySwatch: Colors.red),
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyLarge: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyMedium: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              titleMedium: TextStyle(
+                  fontSize: 20,
+                  fontFamily: "RobotoCondensed",
+                  fontWeight: FontWeight.bold))),
       home: PokemonsPageScreen(),
     );
   }
